b4bc6209eda8d06e3e6b8c7b62afaa8e24d1f99e
mirzda <daudmirzoev@gmail.com>
2018-02-02 12:02:10 +0200

    commit ads

---
+++ rising_up/js/main.js
@@ -0,0 +1,10 @@
+var $form_wrap = $('.form_wrap'),
+$close = $('.close');
+
+setTimeout(function() {
+    $form_wrap.toggleClass("active");
+}, 3000);
+
+$close.click(function(){
+    $form_wrap.hide();
+});
