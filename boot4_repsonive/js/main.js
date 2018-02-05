4097ac98e8703d8955677787a1f426bca6e82e38
mirzda <daudmirzoev@gmail.com>
2018-02-05 16:53:43 +0200

    b4_test

---
+++ boot4_repsonive/js/main.js
@@ -0,0 +1,10 @@
+$('.banner-slider').slick({
+    
+    prevArrow: '<span class="slick-prev fa fa-chevron-left"></span>',
+    nextArrow: '<span class="slick-next fa fa-chevron-right"></span>',
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
